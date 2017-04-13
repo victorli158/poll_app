0efbb181ef8966af526555076834d75321c969bc
Victor Li <victor.li158@gmail.com>
2017-04-13 14:04:20 -0700

    Add User and Poll associations

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,2 +1,10 @@
 class Poll < ApplicationRecord
+  belongs_to :author,
+    class_name: :User,
+    primary_key: :id,
+    foreign_key: :author_id
+  has_many :questions,
+    class_name: :Question,
+    primary_key: :id,
+    foreign_key: :poll_id
 end
