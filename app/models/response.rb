0b3fd5c546a3cf2f0cefa15e2658887e20bbd202
Victor Li <victor.li158@gmail.com>
2017-04-13 15:17:12 -0700

    Add not_author_response validation

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,6 +1,7 @@
 class Response < ApplicationRecord
   validates :user_id, :answer_choice_id, presence: true
   validate :not_duplicate_response
+  validate :not_author_response
 
   belongs_to :user
 
@@ -21,8 +22,17 @@ class Response < ApplicationRecord
     end
   end
 
+  def not_author_response
+    if respondent_is_author?
+      errors[:base] << "You can't respond to your own poll"
+    end
+  end
+
   def respondent_already_answered?
     sibling_responses.exists?(user_id: self.user_id)
   end
 
+  def respondent_is_author?
+    answer_choice.question.poll.author_id == user_id
+  end
 end
