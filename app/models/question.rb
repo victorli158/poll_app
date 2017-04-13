7f0184f63527099b02cae6c4d55d752cd276532b
Victor Li <victor.li158@gmail.com>
2017-04-13 15:53:11 -0700

    Update results method

--- app/models/question.rb
+++ app/models/question.rb
@@ -10,14 +10,29 @@ class Question < ApplicationRecord
     source: :responses
 
   def results
-    answer_choices = self.answer_choices.includes(:responses)
+    # answer_choices = self.answer_choices.includes(:responses)
+    #
+    # result = {}
+    #
+    # answer_choices.each do |choice|
+    #   result[choice.text] = choice.responses.length
+    # end
+    #
+    # result
 
-    result = {}
+    result = Question.find_by_sql(<<-SQL)
+      SELECT
+        ac.*, COUNT(rs.user_id) AS vote
+      FROM
+        answer_choices ac
+      LEFT JOIN
+        responses rs ON ac.id = rs.answer_choice_id
+      WHERE
+        ac.question_id = #{self.id}
+      GROUP BY
+        ac.id
+    SQL
 
-    answer_choices.each do |choice|
-      result[choice.text] = choice.responses.length
-    end
-
-    result
+    result.map { |ac| [ac.text, ac.vote] }.to_h
   end
 end
